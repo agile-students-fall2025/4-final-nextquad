7d3404b62499908c6d784f5ed21f5885ec74d897
lauraliu518 <yixuanliu0518@gmail.com>
2025-10-28 18:42:39 -0400

    file type

---
+++ front-end/src/components/campus_map/Header.js
@@ -0,0 +1,9 @@
+import "./Header.css";
+
+export default function Header() {
+  return (
+    <header className="header">
+      <div className="header-title">Campus Map</div>
+    </header>
+  );
+}
