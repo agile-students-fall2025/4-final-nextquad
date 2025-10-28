28ab53968fae517328f3ad23c447b476593607d4
MilannEngineer <mme7569@nyu.edu>
2025-10-27 23:15:58 -0400

    Updated settings page

--- front-end/src/components/Settings/Settings.js
+++ front-end/src/components/Settings/Settings.js
@@ -2,30 +2,25 @@
 import './Settings.css';
 
 export default function Settings({ navigateTo }) {
+
+  const handleLogout = () => {
+    console.log('User logged out');
+    navigateTo('home');
+  };
+
   return (
     <div className="settings-container">
+      {/* Header */}
       <div className="settings-header">
         <h1>Settings</h1>
       </div>
 
+      {/* Settings Buttons */}
       <div className="settings-content">
-        <h2>Notification Settings</h2>
-
-      </div>
-
-      <div className="settings-content">
-        <h2>Change Password</h2>
-
-      </div>
-
-      <div className="settings-content">
-        <h2>Privacy Policy</h2>
-
-      </div>
-
-      <div className="settings-content">
-        <h2>Log Out</h2>
-
+        <button className="settings-button">Notification Settings</button>
+        <button className="settings-button">Change Password</button>
+        <button className="settings-button">Privacy Policy</button>
+        <button className="logout-button" onClick={handleLogout}>Log Out</button>
       </div>
 
     </div>
