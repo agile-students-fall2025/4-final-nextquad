b5a7baa1db93ca3194e0ef3753c80b30675cf6b0
MilannEngineer <mme7569@nyu.edu>
2025-10-27 22:52:20 -0400

    main settings page

---
+++ front-end/src/components/Settings/Settings.js
@@ -0,0 +1,33 @@
+// import ChangePasswordForm from '../components/ChangePassword';
+import './Settings.css';
+
+export default function Settings({ navigateTo }) {
+  return (
+    <div className="settings-container">
+      <div className="settings-header">
+        <h1>Settings</h1>
+      </div>
+
+      <div className="settings-content">
+        <h2>Notification Settings</h2>
+
+      </div>
+
+      <div className="settings-content">
+        <h2>Change Password</h2>
+
+      </div>
+
+      <div className="settings-content">
+        <h2>Privacy Policy</h2>
+
+      </div>
+
+      <div className="settings-content">
+        <h2>Log Out</h2>
+
+      </div>
+
+    </div>
+  );
+}
