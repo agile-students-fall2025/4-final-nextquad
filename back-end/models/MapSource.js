68fb731dd83650e71bcd728e397a70a34195338b
lauraliu518 <yixuanliu0518@gmail.com>
2025-12-03 19:44:51 -0500

    local data archive

---
+++ back-end/models/MapSource.js
@@ -0,0 +1,70 @@
+const mongoose = require("mongoose");
+
+/**
+ * MapSource model - stores location data with addresses
+ * Coordinates (latitude/longitude) will be geocoded from addresses
+ */
+const mapSourceSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      trim: true
+    },
+    address: {
+      type: String,
+      required: true,
+      trim: true
+    },
+    building: {
+      type: String,
+      default: ''
+    },
+    categories: {
+      type: [String],
+      default: [],
+      validate: {
+        validator: function(v) {
+          const validCategories = ['academic', 'events', 'fitness', 'residential', 'student life', 'study'];
+          return v.every(cat => validCategories.includes(cat));
+        },
+        message: 'Invalid category'
+      }
+    },
+    keywords: {
+      type: [String],
+      default: []
+    },
+    desc: {
+      type: String,
+      default: ''
+    },
+    hours: {
+      type: String,
+      default: ''
+    },
+    link: {
+      type: String,
+      default: ''
+    },
+    // Geocoded coordinates (added when needed)
+    latitude: {
+      type: Number,
+      default: null
+    },
+    longitude: {
+      type: Number,
+      default: null
+    }
+  },
+  { 
+    timestamps: true,
+    collection: 'mapsource' 
+  }
+);
+
+mapSourceSchema.set('toJSON', { virtuals: true });
+mapSourceSchema.set('toObject', { virtuals: true });
+
+module.exports = mongoose.model("MapSource", mapSourceSchema);
+
